d3e480a61c5e6f0ab62b780f27426bfdd4f8acdb
Webmobril <webmobril@Webmobrils-iMac.local>
2019-08-09 15:48:51 +0530

    Install pod in IOS

---
+++ ios/Pods/Target Support Files/Pods-HouseOfDesi/Pods-HouseOfDesi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HouseOfDesi : NSObject
+@end
+@implementation PodsDummy_Pods_HouseOfDesi
+@end
---
+++ ios/Pods/Target Support Files/Pods-HouseOfDesiTests/Pods-HouseOfDesiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HouseOfDesiTests : NSObject
+@end
+@implementation PodsDummy_Pods_HouseOfDesiTests
+@end
